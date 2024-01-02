83614297f9fbc9ec7832b5edf44a60f91de4c2d1
Mikael Gãrtner <Mikael_gartner@hotmail.com>
2024-01-02 18:42:45 +0100

    Test shouldReturnStatusCode 404 (cleanup + comments)

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -9,24 +9,26 @@ public class HttpClient {
     public HttpClient(String host, int port, String requestTarget) throws IOException {
         Socket socket = new Socket(host, port);
 
+        //Her bygger vi opp en request som er kommandoen fra client til server
         String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
                 "Host: " + host + "\r\n" +
                 "Connection: close\r\n" +
                 "\r\n";
-        socket.getOutputStream().write(request.getBytes());
+        socket.getOutputStream().write(request.getBytes()); //Vi sender requesten til serveren
 
-        String[] statusLine = readLine(socket).split(" ");
-        this.statusCode = Integer.parseInt(statusLine[1]);
+        String[] statusLine = readLine(socket).split(" ");  //Leser en linje tilbake fra server og splitter på mellomrom
+        this.statusCode = Integer.parseInt(statusLine[1]);  //Tar det som står mellom første og andre mellomrom, tolker det som et tall
     }
 
+    //For å lese en linje bruker vi en "buffer", en midlertidig struktur hvor jeg bygger opp linja underveis
     private String readLine(Socket socket) throws IOException {
-        StringBuilder result = new StringBuilder();
+        StringBuilder buffer = new StringBuilder();
         int c;
-        while ((c = socket.getInputStream().read()) != '\r') {
-            result.append((char)c);
+        while ((c = socket.getInputStream().read()) != '\r') {  //Leser ett og ett tegn fra socketen inntil vi kommer til \r
+            buffer.append((char)c);
         }
 
-        return result.toString();
+        return buffer.toString();   //Returner så den verdien
     }
 
     public int getStatusCode() {
