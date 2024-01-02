bd730bf668568d33dd51dd5c21f6d75db8d631f7
Mikael Gãrtner <Mikael_gartner@hotmail.com>
2024-01-02 18:24:06 +0100

    Test shouldReturnStatusCode 404 (running green)

--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -2,6 +2,8 @@ package no.kristiania.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
@@ -11,7 +13,7 @@ public class HttpClientTest {
     }
 
     @Test
-    void shouldReturnStatusCode() {
+    void shouldReturnStatusCode() throws IOException {
         assertEquals(200, new HttpClient("httpbin.org", 80, "/html").getStatusCode());
 
         assertEquals(404, new HttpClient("httpbin.org", 80, "/no-such-page").getStatusCode());
