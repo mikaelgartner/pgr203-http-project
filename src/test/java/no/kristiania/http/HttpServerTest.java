3b56ce99f48d3a0a40730898e6219f55b98d9b2d
Mikael Gãrtner <Mikael_gartner@hotmail.com>
2024-01-05 12:42:24 +0100

    Server shouldServeFiles (running red)

--- src/test/java/no/kristiania/http/HttpServerTest.java
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -3,6 +3,10 @@ package no.kristiania.http;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -32,5 +36,17 @@ class HttpServerTest {
                 () -> assertEquals("text/html", client.getHeader("Content-Type")),
                 () -> assertEquals("<p>Hello world</p>", client.getMessageBody())
         );
+    }
+
+    @Test
+    void shouldServeFiles() throws IOException {
+        HttpServer server = new HttpServer(0);
+        server.setRoot(Paths.get("target/test-classes"));
+
+        String fileContent = "A file created at " + LocalTime.now();
+        Files.write(Paths.get("target/test-classes/example-file.txt"), fileContent.getBytes());
+
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/example-file.txt");
+        assertEquals(fileContent, client.getMessageBody());
     }
 }
