255bfb910e57740e26d435d53624f59b1cc77265
Mikael Gãrtner <Mikael_gartner@hotmail.com>
2024-01-22 17:29:03 +0100

    RoleDaoTest should list saved roles (still red but getting even closer)

--- src/test/java/no/kristiania/person/TestData.java
+++ src/test/java/no/kristiania/person/TestData.java
@@ -1,5 +1,6 @@
 package no.kristiania.person;
 
+import org.flywaydb.core.Flyway;
 import org.h2.jdbcx.JdbcDataSource;
 
 import javax.sql.DataSource;
@@ -7,7 +8,8 @@ import javax.sql.DataSource;
 public class TestData {
     public static DataSource testDataSource() {
         JdbcDataSource dataSource = new JdbcDataSource();
-        dataSource.setURL("jdbc:h2:mem:person");
+        dataSource.setURL("jdbc:h2:mem:person;DB_CLOSE_DELAY=-1");
+        Flyway.configure().dataSource(dataSource).load().migrate();
         return dataSource;
     }
 }
