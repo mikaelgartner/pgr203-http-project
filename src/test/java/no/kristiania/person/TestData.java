efc6fc77b0ed98e768bde33e704665df5ecabb57
Mikael Gãrtner <Mikael_gartner@hotmail.com>
2024-01-22 17:13:31 +0100

    RoleDaoTest should list saved roles (still red but getting closer)

--- src/test/java/no/kristiania/person/TestData.java
+++ src/test/java/no/kristiania/person/TestData.java
@@ -1,9 +1,13 @@
 package no.kristiania.person;
 
+import org.h2.jdbcx.JdbcDataSource;
+
 import javax.sql.DataSource;
 
 public class TestData {
     public static DataSource testDataSource() {
-        return null;
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setURL("jdbc:h2:mem:person");
+        return dataSource;
     }
 }
