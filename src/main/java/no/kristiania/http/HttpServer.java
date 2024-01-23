c4e8e90b1049484299c6cb6e47cfd7a08093f0d8
Mikael Gãrtner <Mikael_gartner@hotmail.com>
2024-01-23 11:00:00 +0100

    HttpServer uses RoleDao (cleanup)

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
